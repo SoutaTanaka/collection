847545acc84795e49657a3cb25b579409741fce5
田中颯太 <TanakaSouta@tanakafuuta-no-macbook-pro.local>
2016-06-15 15:59:34 +0900

    Initial Commit

---
+++ collectionview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collectionview
+//
+//  Created by 田中颯太 on 2016/06/15.
+//  Copyright © 2016年 田中颯太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
